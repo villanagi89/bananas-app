d458eda2de2588d1db5586dfc5b7fc4dc08d5c6a
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-08 12:03:54 -0400

    Add #toggle_add_review button to index.html & App.toggleAddReview() to app.js to show/hide the add review form

--- app/js/app.js
+++ app/js/app.js
@@ -20,6 +20,19 @@ App.greet = function(string){
   return string = typeof string !== 'undefined' ? 'Hello ' + string : "Hello World";
 };
 
+App.toggleAddReview = function(){
+  //initial state of add review form is hidden
+  $("#new-review-form").hide();
+
+  //toggle show/hide on click
+  $("#toggle_add_review").click(function(){
+    $("#new-review-form").toggle();
+  });
+};
+
 $(document).ready(function(){
   trace('hello world');
+
+  App.toggleAddReview();
+
 });
